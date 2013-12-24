71db47cf494b8bda426a1e5874c4a31c12b8b638
PCloudTest <1026543901@qq.com>
2013-12-24 00:46:59 -0800

    modified:   bin/boot.sh 	modified:   bin/compile

--- bin/boot.sh
+++ bin/boot.sh
@@ -11,6 +11,46 @@
 # either express or implied. See the License for the specific language governing permissions
 # and  limitations under the License.
 # ------------------------------------------------------------------------------------------------
+compile_nginx_download=https://d35wldypfbhn1h.cloudfront.net/nginx-1.4.1.tar.gz
+
+# ------------------------------------------------------------------------------------------------
+
+compile_build_dir=$1
+compile_cache_dir=$2
+compile_buildpack_dir=$(cd $(dirname $0) && cd .. && pwd)
+compile_buildpack_bin=$compile_buildpack_dir/bin
+
+#echo "pwd: $(pwd)"
+#echo "compile_build_dir: $compile_build_dir"
+#echo "compile_cache_dir: $compile_cache_dir"
+#echo "compile_buildpack_bin: $compile_buildpack_bin"
+#echo "compile_buildpack_dir: $compile_buildpack_dir"
+
+# ------------------------------------------------------------------------------------------------
+
+cd $compile_build_dir
+
+# ------------------------------------------------------------------------------------------------
+
+echo "-----> Doing work with $(basename ${compile_nginx_download%.tar.gz}) son."
+mkdir -p $compile_cache_dir/public
+mv * $compile_cache_dir/public
+mv $compile_cache_dir/public .
+[[ -f public/Procfile ]] && mv public/Procfile .
+curl -s --max-time 60 $compile_nginx_download |tar xz
+
+cp -f $compile_buildpack_dir/conf/nginx.conf nginx/conf/nginx.conf
+cp -f $compile_buildpack_dir/conf/mime.types nginx/conf/mime.types
+
+[[ -f public/nginx.conf ]] && mv public/nginx.conf nginx/conf/nginx.conf
+[[ -f public/mime.types ]] && mv public/mime.types nginx/conf/mime.types
+
+echo "-----> Ensuring manifest.yml & stackato.yml aren't available in public/"
+rm -f public/{manifest,stackato}.yml
+
+
+
+
 
 export APP_ROOT=$HOME
 
